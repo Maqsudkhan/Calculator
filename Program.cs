686dde8c1baa3cc0b70c423cc5fefca919c5e0e5
Maqsudkhan <maqsudtorayev2003@gmail.com>
2023-03-30 02:47:21 +0500

    Oddiygina calculator

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+System.Console.Write("Birinchi sonni kiriting: ");
+string a = System.Console.ReadLine();
+int firstNumber = System.Convert.ToInt32(a);
+System.Console.Write("Ikkinchi sonni kiriting: ");
+string b = System.Console.ReadLine();
+int secondNumber = System.Convert.ToInt32(b);
+
+//   + - * /
+System.Console.WriteLine($"Adding(Qo'shish) {firstNumber + secondNumber}");
+System.Console.WriteLine($"Substracting(Ayirish) {firstNumber - secondNumber}");
+System.Console.WriteLine($"Divide(Bo'lish) {firstNumber / secondNumber}");
+System.Console.WriteLine($"Multiplying(Ko'paytirish) {firstNumber * secondNumber}");
+
+// % qoldiqni chiqaradi
+
+System.Console.WriteLine($"Residual(Qoldiq) {firstNumber % secondNumber}");
